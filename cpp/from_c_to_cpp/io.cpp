79e755d3e4684fa4737e06ff62094ada084ea119
nitkna <nithin.krishna@outlook.com>
2017-09-19 20:48:13 +0530

    an io

---
+++ cpp/from_c_to_cpp/io.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+
+int main(int argc, char **argv){
+
+	int i;
+	std::cout << "please enter an integer value"<<std::endl;
+	std::cin >> i;
+	std::cout << "The value you entered is "<<i << std::endl;
+	return 0;
+}
