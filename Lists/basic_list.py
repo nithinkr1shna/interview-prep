93c4e720a4d53c4f565e1ce30adadff869ac2ec8
nitkna <nithin.krishna@outlook.com>
2017-09-24 14:31:18 +0530

    list basics

---
+++ Lists/basic_list.py
@@ -0,0 +1,8 @@
+x = [1, 2, 3]
+y = ["hello world"]
+z = [1,2,"hello world"] # heterogenous
+print "length of x", len(x)
+print "\n"
+print x[1]
+x[1] = 4
+print x # list is mutable , accessed wit the help of indices
